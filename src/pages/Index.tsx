6295cc389c37b640a1515071cb2f1695625e015c
Ishan-muhammed <ishanelitee@gmail.com>
2025-05-20 14:24:17 +0530

    Your descriptive message for new changes

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -382,11 +382,11 @@ const Index = () => {
             }`}
           >
             <h1 className="text-6xl md:text-6xl font-poppins font-semibold mb-4 md:mb-6 we-gradient-text animate-fade-in drop-shadow-2xl text-left w-full" style={{ color: '#f4f4f2' }}>
-              <span className="block">Tnsform</span>
+              <span className="block">Transform</span>
               <span className="block">your online</span>
               <span className="block">presence with</span>
               <span className="block mt-2 w-full">
-                <Logo className="w-full max-w-[320px] md:max-w-[700px] h-auto align-middle" />
+                <Logo className="w-full max-w-[280px] md:max-w-[500px] h-auto align-middle" />
               </span>
             </h1>
             <p className="text-lg md:text-xl mb-8 md:mb-10 animate-fade-in px-0" style={{ color: '#f4f4f2', animationDelay: '200ms' }}>
