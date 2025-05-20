baba5c02cbdb82b3a8b03234d0c2d2d0ca876b06
Ishan-muhammed <ishanelitee@gmail.com>
2025-05-20 17:55:31 +0530

    hgtyure

--- src/components/ContactDetails.tsx
+++ src/components/ContactDetails.tsx
@@ -4,12 +4,12 @@ export const ContactDetails = () => (
   <div className="w-full h-full flex flex-col justify-center p-6 md:p-10 bg-white rounded-xl gap-10">
     <div className="flex items-center gap-6">
       <div className="flex-shrink-0 rounded-full bg-gradient-to-br from-[#66009d] to-[#9333ea] p-4">
-        <Phone className="w-8 h-8 text-white" />
+        <Phone className="w-10 h-8 text-white" />
       </div>
       <div>
         <div className="text-base text-black/80 font-medium mb-1">Phone</div>
         <div className="font-bold text-1xl text-black">
-          +91 85930 24796 <br /> +91 80862 72916
+          +91 80862 72916 <br /> +91 8593024796
         </div>
       </div>
     </div>
